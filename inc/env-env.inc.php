2b48aff68cf284386ad0ff0de5978e8b143fa95e
LaetitiaBodin <laetitia.bodin.webdev@gmail.com>
2023-03-01 10:11:20 +0100

    chore: setup

---
+++ inc/env-env.inc.php
@@ -0,0 +1,7 @@
+<?php
+    $_ENV['host'] = 'localhost';
+    $_ENV['dbname'] = 'to_do_app';
+    $_ENV['tname'] = 'todos';
+    $_ENV['user'] = 'root';
+    $_ENV['password'] = '';
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To-Do App</title>
+    <link rel="shortcut icon" href="img/favicon.svg" type="image/x-icon">
+    <!-- Tailwind CSS -->
+    <link rel="stylesheet" href="style/tailwind-out.css">
+    <!-- Javascript -->
+    <script src="script/main.js" type="module" defer></script>
+</head>
+<body class="bg-teal-500 font-sans">
+    <h1>To-Do App</h1>
+</body>
+</html>
